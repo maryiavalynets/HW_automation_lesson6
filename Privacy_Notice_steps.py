00f2945ecbaef849d1cbaf8450be4bebd61b40d2
Maryia Valynets <maryia.valynets@gmail.com>
2022-10-20 11:33:41 -0700

    Add Privacy_Notice_steps.py

---
+++ Privacy_Notice_steps.py
@@ -0,0 +1,38 @@
+from selenium.webdriver.common.by import By
+from behave import given, when, then
+from selenium.webdriver.support import expected_conditions as EC
+
+PRIVACY_NOTICE_LINK = (By.CSS_SELECTOR, "a[href = 'https://www.amazon.com/privacy']")
+
+
+@given('Open Amazon T&C page')
+def open_T_C_page(context):
+    context.driver.get('https://www.amazon.com/gp/help/customer/display.html/ref=ap_register_notification_condition_of_use?ie=UTF8&nodeId=508088')
+
+
+@when('Store original windows')
+def store_original_window(context):
+    context.original_window = context.driver.current_window_handle
+
+
+@when('Click on Amazon Privacy Notice link')
+def click_pvivacy_notice_link(context):
+    context.driver.find_element(*PRIVACY_NOTICE_LINK).click()
+
+
+@when('Switch to the newly opened window')
+def switch_new_open_window(context):
+    context.driver.wait.until(EC.new_window_is_opened)
+    current_windows = context.driver.window_handles
+    context.driver.switch_to.window(current_windows[1])
+
+
+@then('Verify Amazon Privacy Notice page is opened')
+def verify_Privacy_Notice_page_open(context):
+    context.driver.wait.until(EC.url_contains('https://www.amazon.com/gp/help/customer/display.html?nodeId=GX7NJQ4ZB8MHFRNJ'))
+
+
+@then('User can close new window and switch back to original')
+def close_new_window_switch_original(context):
+    context.driver.close()
+    context.driver.switch_to.window(context.original_window)
